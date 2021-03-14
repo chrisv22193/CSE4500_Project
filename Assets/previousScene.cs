2f39dc752c7bc59b23535f341d3f3276a0c85ddf
Chris Vazquez <006854322@coyote.csusb.edu>
2021-03-13 19:12:27 -0800

    Uploaded project

---
+++ Assets/previousScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class previousScene : MonoBehaviour{
+
+    private int previousSceneToLoad;
+
+
+    // Start is called before the first frame update
+    void Start(){
+        previousSceneToLoad = SceneManager.GetActiveScene().buildIndex - 1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        SceneManager.LoadScene(previousSceneToLoad);
+    }
+}
